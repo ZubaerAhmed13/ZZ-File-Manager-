1e011ed32e07f16e5e38ff384eccfe8ab907fa05
Zubaer Ahmed <zubaerknight@gmail.com>
2026-09-08 21:35:14 +0200

    test: fix Compose interaction test imports

--- app/src/androidTest/java/com/zz/filemanager/Step2BrowserUiTest.kt
+++ app/src/androidTest/java/com/zz/filemanager/Step2BrowserUiTest.kt
@@ -2,8 +2,6 @@ package com.zz.filemanager
 
 import android.content.Context
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.test.assertDoesNotExist
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.longClick
 import androidx.compose.ui.test.onAllNodesWithText
