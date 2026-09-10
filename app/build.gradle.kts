c283332b4e7dc657211d765f9f3e5b495dc5954f
Zubaer Ahmed <zubaerknight@gmail.com>
2026-09-10 16:12:57 +0200

    test(step5): use Android-compatible disposable HTTP server

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,6 +90,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
     testImplementation("org.json:json:20240303")
+    // Android/JVM-compatible disposable HTTP server used to exercise the production WebDAV
+    // client without relying on JDK-internal com.sun.net.httpserver classes.
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
 
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
