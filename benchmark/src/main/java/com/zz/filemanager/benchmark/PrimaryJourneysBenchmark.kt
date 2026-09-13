f47ae549a5f9b3f686308cbdfe6422f207774e94
Zubaer Ahmed <zubaerknight@gmail.com>
2026-09-13 10:13:39 +0200

    Fix macrobenchmark rule imports

--- benchmark/src/main/java/com/zz/filemanager/benchmark/PrimaryJourneysBenchmark.kt
+++ benchmark/src/main/java/com/zz/filemanager/benchmark/PrimaryJourneysBenchmark.kt
@@ -2,13 +2,12 @@ package com.zz.filemanager.benchmark
 
 import androidx.benchmark.macro.*
 import androidx.benchmark.macro.junit4.BaselineProfileRule
+import androidx.benchmark.macro.junit4.MacrobenchmarkRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.LargeTest
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@LargeTest
 @RunWith(AndroidJUnit4::class)
 class PrimaryJourneysBenchmark {
     @get:Rule val benchmark = MacrobenchmarkRule()
@@ -23,7 +22,6 @@ class PrimaryJourneysBenchmark {
     companion object { private const val PACKAGE = "com.zz.filemanager" }
 }
 
-@LargeTest
 @RunWith(AndroidJUnit4::class)
 class PrimaryJourneysBaselineProfile {
     @get:Rule val baseline = BaselineProfileRule()
